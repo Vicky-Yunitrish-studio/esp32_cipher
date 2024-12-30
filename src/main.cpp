1626470e57223111093b28a4cfe8811e388eab94
yunitrish <yunthomas006@gmail.com>
2024-12-30 15:28:45 +0800

    Implement NTP time synchronization and toggle LED based on WiFi connection status; clean up library dependencies in platformio.ini

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <WiFi.h>
+#include <time.h>
 
-char *ssid = "Yun";
+const char *ssid = "Yun";
 const char *password = "0937565253";
+
+const char* ntpServer = "tock.stdtime.gov.tw";
+const long gmtOffset_sec = 8 * 3600;    // 台灣 GMT+8
+const int daylightOffset_sec = 0; 
+time_t recordedTime = 0;
+time_t currentTime = 0;
 
 int btnGPIO = 0;
 int btnState = false;
@@ -46,6 +53,14 @@ void setup() {
         Serial.println("[WiFi] WiFi is connected!");
         Serial.print("[WiFi] IP address: ");
         Serial.println(WiFi.localIP());
+        configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+        struct tm tmp;
+        if (getLocalTime(&tmp)) {
+          recordedTime = mktime(&tmp);
+          Serial.printf("Initial Recorded Time: %ld\n", recordedTime);
+        } else {
+          Serial.println("Failed to obtain initial time");
+        }
         return;
         break;
       default:
@@ -65,23 +80,39 @@ void setup() {
     }
   }
 }
-
+bool light_status = false;
 void loop() {
-  // Read the button state
   btnState = digitalRead(btnGPIO);
 
+  struct tm tmp;
+  if (getLocalTime(&tmp)) {
+    currentTime = mktime(&tmp);
+  } else {
+    Serial.println("Failed to obtain current time");
+    delay(1000);
+    return;
+  }
+
+  if (difftime(currentTime, recordedTime) >= 5) {
+    recordedTime = currentTime;
+    if (WiFi.status() == WL_CONNECTED) {
+      if (light_status) {
+        digitalWrite(LED_BUILTIN, HIGH);
+      }
+      else {
+        digitalWrite(LED_BUILTIN, LOW);
+      }
+      light_status = !light_status;
+    }
+  }
+  
   if (btnState == LOW) {
     // Disconnect from WiFi
     Serial.println("[WiFi] Disconnecting from WiFi!");
     // This function will disconnect and turn off the WiFi (NVS WiFi data is kept)
-    if (WiFi.disconnect()) {
+    if (WiFi.disconnect(true, false)) {
       Serial.println("[WiFi] Disconnected from WiFi!");
     }
     delay(1000);
   }
-
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);    
 }
